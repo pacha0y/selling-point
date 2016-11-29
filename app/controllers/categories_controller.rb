33da6047e0e2c248c817fdb6196b215d5fc97439
Pachawo Bisani <phbisan@gmail.com>
2016-11-29 07:08:30 +0000

    Created a shared view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,7 +16,7 @@ class CategoriesController < ApplicationController
         # render plain:params[:category].inspect
         @category = Category.new(category_params)
         if @category.save
-            flash[:notice] = "Category was successfully created"
+            flash[:success] = "Category was successfully created"
             redirect_to category_path(@category)
         else
             render 'new'
@@ -25,7 +25,7 @@ class CategoriesController < ApplicationController
     
     def update
         if @category.update(category_params)
-            flash[:notice] = "Category was successfully updated"
+            flash[:success] = "Category was successfully updated"
             redirect_to category_path(@category)
         else
             render 'edit'
@@ -37,7 +37,7 @@ class CategoriesController < ApplicationController
     
     def destroy
         @category.destroy
-        flash[:notice] = "Category was successfully deleted"
+        flash[:danger] = "Category was successfully deleted"
         redirect_to categories_path
     end
     
